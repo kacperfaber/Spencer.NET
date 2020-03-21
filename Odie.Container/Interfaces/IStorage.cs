cfad7cb6e97c1947518f2493be759a4055994c83
Kacper Faber <kacperf1234@gmail.com>
2020-03-21 18:12:32 +0100

    created implementation of IStorage.

---
+++ Odie.Container/Interfaces/IStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Odie
+{
+    public interface IStorage
+    {
+        IServiceList Services { get; set; }
+        
+        IAssemblyList Assemblies { get; set; }
+    }
+}
---
+++ Odie.Container/Models/Storage.cs
@@ -0,0 +1,14 @@
+﻿namespace Odie
+{
+    public class Storage : IStorage
+    {
+        public Storage()
+        {
+            Services = new ServiceList();
+            Assemblies = new AssemblyList();
+        }
+        
+        public IServiceList Services { get; set; }
+        public IAssemblyList Assemblies { get; set; }
+    }
+}
