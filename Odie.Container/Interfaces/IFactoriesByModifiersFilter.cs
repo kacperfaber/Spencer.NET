d7355b6decd023809746febc1bcfa44aed8ae4b0
Kacper Faber <kacperf1234@gmail.com>
2020-03-24 14:30:26 +0100

    Added FactoriesByModifiersFilter. He have to have test. Now it is not will be invoked.

---
+++ Odie.Container/Interfaces/IFactoriesByModifiersFilter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Odie
+{
+    public interface IFactoriesByModifiersFilter
+    {
+        IEnumerable<IFactory> Filter(IEnumerable<IFactory> factories);
+    }
+}
---
+++ Odie.Container/Types/FactoriesByModifiersFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Odie
+{
+    public class FactoriesByModifiersFilter : IFactoriesByModifiersFilter
+    {
+        public IEnumerable<IFactory> Filter(IEnumerable<IFactory> factories)
+        {
+            foreach (IFactory factory in factories)
+            {
+                if (factory.Member is MethodInfo method)
+                {
+                    if (!method.IsGenericMethod && method.IsStatic)
+                        yield return factory;
+                }
+                
+                if (factory.Member is FieldInfo field)
+                {
+                    if (field.IsStatic)
+                        yield return factory;
+                }
+                
+                if (factory.Member is PropertyInfo property)
+                {
+                    if (property.GetGetMethod() != null && property.GetAccessors().Any(x => x.IsStatic))
+                        yield return factory;
+                }
+            }
+            
+            // TODO test, now it does not putted to system.
+        }
+    }
+}
