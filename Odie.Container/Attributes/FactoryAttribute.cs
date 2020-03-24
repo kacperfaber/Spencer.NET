168f720f893436a06d45b95ff1448cf645472cb7
Kacper Faber <kacperf1234@gmail.com>
2020-03-24 10:36:09 +0100

    added FactoryAttribute

---
+++ Odie.Container/Attributes/FactoryAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Odie
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public class FactoryAttribute : ServiceFlagAttribute
+    {
+        public FactoryAttribute() : base(ServiceFlagConstants.ServiceFactory)
+        {
+        }
+    }
+}
