e806a9e08934243af3f4d26acc83a04dbe44bd49
Kacper Faber <kacperf1234@gmail.com>
2020-04-28 11:01:02 +0200

    Added tests for StorageBuilder Build Register

---
+++ Spencer.NET.Tests/storagebuilder_build_tests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Spencer.NET.Tests
+{
+    public class storagebuilder_build_tests
+    {
+        class TestClass
+        {
+        }
+        
+        object exec(StorageBuilder builder)
+        {
+            return builder.Build();
+        }
+
+        [Test]
+        public void dont_throws_exceptions()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            
+            Assert.DoesNotThrow(() => exec(builder));
+        }
+
+        [Test]
+        public void returns_instance_of_Storage()
+        {
+            StorageBuilder builder = new StorageBuilder(); 
+            
+            Assert.IsInstanceOf<Storage>(builder.Build());
+        }
+
+        [Test]
+        public void returns_object_is_IStorage()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            
+            Assert.IsTrue(builder.Build() is IStorage);
+        }
+
+        [Test]
+        public void returns_not_null()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            
+            Assert.NotNull(builder.Build());
+        }
+
+        [Test]
+        public void returns_object_is_equals_to_builder_Object()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            
+            Assert.AreEqual(builder.Object, builder.Build());
+        }
+
+        [Test]
+        public void returns_excepted_services_count()
+        {
+            StorageBuilder builder = new StorageBuilder().Register<Tests.TestClass>().Register<TestClass>();
+            
+            Assert.IsTrue(builder.Build().Services.GetServices().Count() == 2);
+        }
+    }
+}
---
+++ Spencer.NET.Tests/storagebuilder_register_tests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Spencer.NET.Tests
+{
+    public class storagebuilder_register_tests
+    {
+        interface ITestClass
+        {
+        }
+
+        class TestClass : ITestClass
+        {
+        }
+
+        void exec(StorageBuilder builder, Type type)
+        {
+            builder.Register(type);
+        }
+
+        [Test]
+        public void dont_throws_exceptions()
+        {
+            Assert.DoesNotThrow(() => exec(new StorageBuilder(), typeof(TestClass)));
+        }
+
+        [Test]
+        public void builder_has_Object_not_null()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+            
+            Assert.NotNull(builder.Object);
+        }
+
+        [Test]
+        public void builder_Object_is_instance_of_Storage()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+            
+            Assert.IsInstanceOf<Storage>(builder.Object);
+        }
+
+        [Test]
+        public void builder_Object_is_assignable_to_IStorage()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+
+            Assert.IsTrue(builder.Object is IStorage);
+        }
+
+        [Test]
+        public void builder_Object_Services_not_null()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+
+            Assert.NotNull(builder.Object.Services);
+        }
+
+        [Test]
+        public void builder_Object_Services_count_is_one()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+
+            Assert.IsTrue(builder.Object.Services.GetServices().Count() == 1);
+        }
+
+        [Test]
+        public void builder_Object_Services_is_not_empty_after_Register()
+        {
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, typeof(TestClass));
+
+            Assert.IsNotEmpty(builder.Object.Services.GetServices());
+        }
+
+        [Test]
+        public void builder_Object_Services_contains_TestClass()
+        {
+            Type testClassType = typeof(TestClass);
+            
+            StorageBuilder builder = new StorageBuilder();
+            exec(builder, testClassType);
+
+            Assert.NotNull(builder.Object.Services.GetServices().FirstOrDefault(x => x.Registration.TargetType == testClassType));
+        }
+    }
+}
