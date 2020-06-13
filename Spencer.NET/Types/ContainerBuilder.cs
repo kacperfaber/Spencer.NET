d40a3cf7c0207c926949887e6766729213c06ead
Kacper Faber <kacperf1234@gmail.com>
2020-06-13 12:01:30 +0200

    Added ContainerBuilder with 5 void methods.

---
+++ Spencer.NET/Types/ContainerBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+
+namespace Spencer.NET
+{
+    public class ContainerBuilder
+    {
+        public void RegisterClass<T>() where T : class
+        {
+        }
+
+        public void RegisterInterface<T>()
+        {
+        }
+
+        public void RegisterAssembly(Assembly assembly)
+        {
+        }
+
+        public void Register(Type type)
+        {
+        }
+
+        public void Register<T>()
+        {
+        }
+    }
+}
