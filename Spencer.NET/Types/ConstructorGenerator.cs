1b404d3aa0e092fbdb5de782473939d498d0b34c
Kacper Faber <kacperf1234@gmail.com>
2023-07-24 17:22:11 +0200

    ConstructorGenerator.cs
    Returns null if given 'constructor: ConstructorInfo' is null.

--- Spencer.NET/Types/ConstructorGenerator.cs
+++ Spencer.NET/Types/ConstructorGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Spencer.NET
 {
@@ -11,11 +13,20 @@ namespace Spencer.NET
             ParametersGenerator = parametersGenerator;
         }
 
+        /// <summary>
+        /// Makes IConstructor using given ConstructorInfo, or null when constructor given is null.
+        /// </summary>
+        /// <param name="constructor"></param>
+        /// <returns>null, if 'constructor' given is null.</returns>
         public IConstructor GenerateConstructor(ConstructorInfo constructor)
         {
+            if (constructor == null) return null;
+            
+            var parameters = ParametersGenerator.GenerateParameters(constructor.GetParameters());
+            
             return new ConstructorBuilder()
                 .AddInstance(constructor)
-                .AddParameters(ParametersGenerator.GenerateParameters(constructor.GetParameters()))
+                .AddParameters(parameters)
                 .Build();
         }
     }
